6fff7001449cde607a8b9ff8ff9ded38624fde6c
Mikhail Metrikin <metrikin@gmail.com>
2017-02-26 17:10:59 -0800

    Random Q's and populating approriate labels

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,53 @@
-
-
 var questionsList = [
   {
-    question: "This is a question?",
+    questionMain: "This is a questionMain?",
     answers: ["Answer1", "Answer2", "Answer3", "Answer4"]
   },
   {
-    question: "Here is another question? Possibly?",
+    questionMain: "Here is another questionMain? Possibly?",
     answers: ["Answer1", "Answer2", "Answer3", "Answer4"]
   },
   {
-    question: "Ok, let's just pretend that these here are real questions. Ok?",
+    questionMain: "Ok, let's just pretend that these here are real questions. Ok?",
     answers: ["Answer1", "Answer2", "Answer3", "Answer4"]
   }
 ];
 
+function randomQuestion() {
+  return questionsList[Math.floor(Math.random() * questionsList.length)];
+}
+
+function buildAnswer(answer) {
+
+  var answerBox = $('<label class="checkbox">');
+  answerBox.append("<input type='checkbox'>").text(answer);
+
+  return answerBox;
+
+  //<input type="checkbox" id="inlineCheckbox1" value="option1"> Internals
+}
+
+function buildQuestion() {
+  var questionObj = randomQuestion();
+
+  $('#question-well').html(questionObj.questionMain);
+  $('#question-box1').empty();
+  $('#question-box2').empty();
+
+
+  for(var i = 0; i < questionObj.answers.length/2; i++){
+
+    $('#question-box1').append(buildAnswer(questionObj.answers[i]));
+    $('#question-box2').append(buildAnswer(questionObj.answers[i + 2]))
+
+  }
+
+}
+
 
 $(document).ready(function () {
 
-  $('#question-well').html(questionsList[0].question);
+  buildQuestion();
 
 
 
